0f8fc5836ba63d72087376bd5f21f5ff1aef25d9
David Molineus <david.molineus@netzmacht.de>
2019-06-03 13:54:39 +0200

    Insert conwegis modules right after the content section.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
-$GLOBALS['BE_MOD']['cowegis'] = [
-    'cowegis_geocode' => [
-        'tables' => ['tl_cowegis_geocoder_provider'],
-    ],
-];
+array_insert(
+    $GLOBALS['BE_MOD'],
+    1,
+    [
+        'cowegis' => [
+            'cowegis_geocoder' => [
+                'tables' => ['tl_cowegis_geocoder_provider'],
+            ],
+        ]
+    ]
+);
 
 if (TL_MODE === 'BE') {
     $GLOBALS['TL_CSS'][] = 'bundles/cowegiscontaogeocode/css/backend.css|static';
