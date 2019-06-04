e18bc373930e3f2ff3534574ab188c85dbec1d16
David Molineus <david.molineus@netzmacht.de>
2019-06-04 14:17:39 +0200

    Fix name of extension.

--- src/DependencyInjection/CowegisContaoGeocoderExtension.php
+++ src/DependencyInjection/CowegisContaoGeocoderExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class CowegisContaoGeocodeExtension extends Extension
+final class CowegisContaoGeocoderExtension extends Extension
 {
     /** @param mixed[][] $configs */
     public function load(array $configs, ContainerBuilder $container) : void
