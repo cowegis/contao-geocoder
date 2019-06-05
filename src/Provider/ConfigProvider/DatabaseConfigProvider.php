17ca430cf012a6c4eeaee5d758e6058e58cfe179
David Molineus <david.molineus@netzmacht.de>
2019-06-05 12:04:58 +0200

    Avoid that unsupported configuration types are loaded.

--- src/Provider/ConfigProvider/DatabaseConfigProvider.php
+++ src/Provider/ConfigProvider/DatabaseConfigProvider.php
@@ -7,22 +7,39 @@ namespace Cowegis\ContaoGeocoder\Provider\ConfigProvider;
 use ArrayIterator;
 use Cowegis\ContaoGeocoder\Model\ProviderRepository;
 use Cowegis\ContaoGeocoder\Provider\ConfigProvider;
+use Cowegis\ContaoGeocoder\Provider\ProviderFactory;
 use IteratorAggregate;
+use function count;
+use function sprintf;
+use function str_repeat;
 
 final class DatabaseConfigProvider implements IteratorAggregate, ConfigProvider
 {
     /** @var ProviderRepository */
     private $repository;
 
-    public function __construct(ProviderRepository $repository)
+    /** @var ProviderFactory */
+    private $providerFactory;
+
+    public function __construct(ProviderRepository $repository, ProviderFactory $providerFactory)
     {
-        $this->repository = $repository;
+        $this->repository      = $repository;
+        $this->providerFactory = $providerFactory;
     }
 
     /** @return mixed[][] */
     public function getIterator() : iterable
     {
-        $collection = $this->repository->findAll();
+        $types = $this->providerFactory->typeNames();
+        if (count($types) === 0) {
+            return new ArrayIterator();
+        }
+
+        $collection = $this->repository->findBy(
+            [sprintf('.type IN (?%s)', str_repeat(',?', count($types) - 1))],
+            $types
+        );
+
         if ($collection) {
             return new ArrayIterator($collection->fetchAll());
         }
